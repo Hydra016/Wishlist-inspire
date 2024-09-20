16c18fabb2e99d6ee58ebd920e7f26ef21a31ca4
purplefire-dev <development@purplefire.io>
2024-09-21 00:54:03 +0300

    first commit

---
+++ app/routes/app.pricing.jsx
@@ -0,0 +1,20 @@
+import {
+    Box,
+    Card,
+    Layout,
+    Link,
+    List,
+    Page,
+    Text,
+    BlockStack,
+  } from "@shopify/polaris";
+  import { TitleBar } from "@shopify/app-bridge-react";
+  
+  export default function Pricing() {
+    return (
+      <Page>
+        <TitleBar title="Pricing" />
+      </Page>
+    );
+  }
+  
